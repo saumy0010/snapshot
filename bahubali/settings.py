c563bb9a0971bf6f6a05ce8ed63a756f8b0f55ec
sp8795 <shubhamspandey470@gmail.com>
2021-03-24 22:53:35 +0530

    second commit

--- bahubali/settings.py
+++ bahubali/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ify-1)v2t2!u^n&m#!y!5t%wk*wt8*r5s782beu)o^f7^%@f7z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
